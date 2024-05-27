52a94d0fe3e0e9b4bb524f368c1744131924a64f
Pat <pat_boo61@tbs.tu.ac.th>
2024-05-27 15:47:47 +0700

    Complete exercises 1 and 2

--- exercise_02/App.jsx
+++ exercise_02/App.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 
+function Header() {
+  return (
+    <header>
+      <button>ON/OFF</button>
+      <span>Current Temperature: 25C</span>
+    </header>
+  )
+}
+
+function Content(props) {
+  return (
+    <main>
+      <Temperature/>
+    </main>
+  )
+}
+
+function Temperature(props) {
+  return <span>25C</span>;
+}
+
+function Footer() {
+  return (
+    <footer>
+      <button>Down</button>
+      <button>Up</button>
+    </footer>
+  )
+}
+
 const App = () => {
   const temperature = 25;
 
-	return <>{/* code here */};</>;
+	return (
+    <>
+      <Header />
+      <Content />
+      <Footer/>
+    </>
+  );
 };
 
 export default App;
